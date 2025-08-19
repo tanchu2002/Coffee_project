5c20ef196245c4e9d6f97b4bdeaf8271297e845a
ustrungnguyen <contact@ngtrung.com>
2025-08-20 00:24:36 +0700

    Updated product gallery

--- ProductDetail/js/product-gallery-render.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () =>{
-    const mainImageContainer = document.querySelector('.js-main-image');
-    const subImagesContainer = document.querySelector('.js-sub-images-gallery');
-
-    const mainImage = document.createElement('img');
-});
--- ProductDetail/js/product-image-select.js
+++
@@ -1,8 +0,0 @@
-let mainImage = document.getElementsByClassName('js-main-image');
-let subImages = document.querySelectorAll('.js-image-selector');
-
-subImages.forEach(subImage => {
-    subImage.addEventListener('click', () => {
-        mainImage.src = subImage.src;
-    });
-});
