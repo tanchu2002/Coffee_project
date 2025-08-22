d5d17d1d7d764258005528ca5469cf45c7ed1faa
ustrungnguyen <contact@ngtrung.com>
2025-08-23 02:03:27 +0700

    Updated Product Detail and fixed Footer

--- ProductDetail/js/product-tabs.js
+++ ProductDetail/js/product-tabs.js
@@ -1,22 +1,39 @@
+// Importing HTML element
 import { renderDescriptionTab } from './utils/detailed-description-tab.js';
 import { renderSpecificationsTab } from './utils/specifications-tab.js';
 import { renderReviewsTab } from './utils/review-tab.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Query required HTML element by className
     const descriptionButton = document.querySelector('.js-detailed-description-tab-button');
     const specificationsButton = document.querySelector('.js-specifications-tab-button');
     const reviewButton = document.querySelector('.js-review-tab-button');
     const tabContainer = document.querySelector('.product-tab-root');
+    const tabButtons = document.querySelectorAll('.tab-button');
 
+    // Detailed description button
     descriptionButton.addEventListener('click', () => {
         tabContainer.innerHTML = renderDescriptionTab();
     });
 
+    // Specifications button
     specificationsButton.addEventListener('click', () => {
         tabContainer.innerHTML = renderSpecificationsTab();
     });
 
+    // Review button
     reviewButton.addEventListener('click', () => {
+
         tabContainer.innerHTML = renderReviewsTab();
+    });
+
+    tabButtons.forEach(clickedButton => {
+        clickedButton.addEventListener('click', () => {
+            tabButtons.forEach(button => {
+                button.classList.remove('button-clicked');
+            });
+
+            clickedButton.classList.add('button-clicked');
+        });
     });
 });
